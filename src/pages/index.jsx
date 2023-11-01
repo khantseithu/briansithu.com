bf1a3845c94b347b71c9727e4422d8cb712e7ee3
Khant Sithu <khantseithuu@gmail.com>
2023-11-01 22:03:23 +0630

    chore: update heading

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -374,7 +374,7 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software devloper, freelancer, and innovator.
+            Software Developer, make stuffs
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I'm Khant Sithu, a versatile Full Stack Developer with a passion for
