939774902504702ed2c7fa906252b7fbbc12d7ab
Khant Sithu <khantseithuu@gmail.com>
2024-01-14 16:11:26 +0000

    add a sitemap

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -290,7 +290,7 @@ function Education() {
       logo: logoFacebook,
       start: '2022',
       end: '2023',
-      link: "https://www.credly.com/badges/168f5e11-4ca3-4a64-81b4-bb937971ecc2"
+      link: "https://www.credly.com/badges/d0d567cd-8640-40e0-a885-479e16062828"
     },
     {
       school: 'Meta',
@@ -384,7 +384,7 @@ export default function Home({ articles }) {
   return (
     <>
       <Head>
-        <title>Khant Sithu - Software developer</title>
+        <title>Khant Sithu</title>
         <meta
           name="description"
           content="I'm Khant Sithu, a passionate CS student and freelance software developer from Myanmar. With expertise in MERN Stack, TypeScript, Next.js, and Tailwind CSS, I bring ideas to life through code, delivering high-quality software solutions. Explore my portfolio to discover innovative projects and collaborations."
@@ -393,7 +393,7 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Khant Sithu
+            Hi, I'm Khant Sithu.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
           Embracing the indie hacker spirit, I navigate the coding landscape with determination. Every project is a step closer to unlocking new heights of innovation and self-discovery.
