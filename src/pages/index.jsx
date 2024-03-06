9887cf0f1b3b75d8039bf39ee2a089b1a51cb62a
Khant Sithu <khantseithuu@gmail.com>
2024-03-06 10:13:35 +0000

    update cv

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -251,12 +251,12 @@ function Resume() {
           </li>
         ))}
       </ol>
-      <a href="/khantsithu.pdf" download>
+      <a href="https://cv.khantsithu.me" target='_blank'>
         <div
           href="#"
           className="group mt-6 inline-flex w-full items-center justify-center gap-2 rounded-md bg-zinc-50 px-3 py-2 text-sm font-medium text-zinc-900 outline-offset-2 transition hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 active:transition-none dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70"
         >
-          Download CV
+          Visit CV
           <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
         </div>
       </a>
